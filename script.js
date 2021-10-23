0a5b8035a12549571e5943f10eda0f949e47d16a
Asskicker4Y <imziyasser00@gmail.com>
2021-10-23 15:32:02 +0100

    final commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var sp = document.getElementsByTagName("span");
 var btn_start = document.getElementById("Start");
 var btn_stop = document.getElementById("Stop");
 var t;
-var ms = 0, s = 0, mn =1, h = 0;
+var ms = 0, s = 0, mn =0, h = 0;
 
 function Start(){
     t = setInterval(update_chrono,100);
@@ -19,14 +19,30 @@ function update_chrono(){
         s = 0;
         mn+=1;
     }
-    if(mn = 60){
+    if(mn == 60){
         mn = 0;
         h+=1;
     }
-    sp[3].innerHTML = ms ;
-    sp[2].innerHTML = s ;
-    sp[0].innerHTML = mn ;
+    sp[3].innerHTML = ms + " ms" ;
+    sp[2].innerHTML = s + " s  ";
+    sp[1].innerHTML = mn + " mn  " ;
+    sp[0].innerHTML = h +" h ";
 
 }
 
+function Stop() {
+    clearInterval(t);
+    btn_start.disabled = false;
+}
 
+function Reset() {
+    ms = 0;
+    s = 0;
+    mn = 0;
+    h = 0;
+
+    sp[3].innerHTML = ms + "ms" ;
+    sp[2].innerHTML = s + "s  ";
+    sp[1].innerHTML = mn + "mn  " ;
+    sp[0].innerHTML = h +"h ";
+}
